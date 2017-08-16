8ebdc6406566146e888defbb8b2f64663012a419
Alexey Golub <Tyrrrrrr@gmail.com>
2017-08-16 23:13:39 +0300

    Add project files.

---
+++ Readme.md
@@ -0,0 +1,32 @@
+# YoutubeDownloader
+
+Command line executable that can download Youtube videos or entire playlists.
+
+## Usage examples
+
+- `YoutubeDownloader.exe "https://www.youtube.com/watch?v=I7RHr4o7t7E"` (video by URL)
+- `YoutubeDownloader.exe I7RHr4o7t7E` (video by ID)
+- `YoutubeDownloader.exe "https://www.youtube.com/watch?v=0KSOMA3QBU0&list=PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj"` (playlist by URL)
+- `YoutubeDownloader.exe PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj` (playlist by ID)
+
+You can include multiple URLs or IDs simply by separating the arguments with space, like so:
+
+- `YoutubeDownloader.exe I7RHr4o7t7E 05nwADM_X-U ucDCFsiN5Gc` (multiple videos by ID)
+
+Or you can also combine the URLs with IDs:
+
+- `YoutubeDownloader.exe "https://www.youtube.com/watch?v=I7RHr4o7t7E" 05nwADM_X-U ucDCFsiN5Gc` (multiple videos by ID)
+
+Or even combine playlists with videos:
+
+- `YoutubeDownloader.exe I7RHr4o7t7E PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj` (video by ID and playlist by ID)
+
+## Libraries used
+
+- [YoutubeExplode](https://github.com/Tyrrrz/YoutubeExplode)
+- [CliWrap](https://github.com/Tyrrrz/CliWrap)
+- [Tyrrrz.Extensions](https://github.com/Tyrrrz/Extensions)
+
+## Runtimes used
+
+- [FFMPEG](https://ffmpeg.org)
---
+++ YoutubeDownloader/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Tyrrrz.Extensions;
+using System.Threading.Tasks;
+using CliWrap;
+using YoutubeExplode;
+using YoutubeExplode.Models;
+using YoutubeExplode.Models.MediaStreams;
+
+namespace YoutubeDownloader
+{
+    public class Program
+    {
+        private static readonly YoutubeClient YoutubeClient = new YoutubeClient();
+        private static readonly Cli FfmpegCli = new Cli("ffmpeg.exe");
+        private static readonly string OutputDir = Path.Combine(Directory.GetCurrentDirectory(), "Output");
+
+        private static MediaStreamInfo GetBestVideoStreamInfo(VideoInfo videoInfo)
+        {
+            if (videoInfo.VideoStreams.Any())
+                return videoInfo.VideoStreams.OrderBy(s => s.VideoQuality).ThenBy(s => s.Bitrate).Last();
+            throw new Exception("No applicable media streams found for this video");
+        }
+
+        private static MediaStreamInfo GetBestAudioStreamInfo(VideoInfo videoInfo)
+        {
+            if (videoInfo.AudioStreams.Any())
+                return videoInfo.AudioStreams.OrderBy(s => s.Bitrate).Last();
+            throw new Exception("No applicable media streams found for this video");
+        }
+
+        private static async Task DownloadVideoAsync(string id)
+        {
+            Console.WriteLine($"Working on video [{id}]...");
+
+            // Get video info
+            var videoInfo = await YoutubeClient.GetVideoInfoAsync(id);
+            string cleanTitle = videoInfo.Title.Except(Path.GetInvalidFileNameChars());
+            Console.WriteLine($"{videoInfo.Title}");
+
+            // Get best streams
+            var videoStreamInfo = GetBestVideoStreamInfo(videoInfo);
+            var audioStreamInfo = GetBestAudioStreamInfo(videoInfo);
+
+            // Download streams
+            Console.WriteLine("Downloading...");
+            Directory.CreateDirectory(OutputDir);
+            string videoStreamFileExt = videoStreamInfo.Container.GetFileExtension();
+            string videoStreamFilePath = Path.Combine(OutputDir, $"VID-{Guid.NewGuid()}.{videoStreamFileExt}");
+            await YoutubeClient.DownloadMediaStreamAsync(videoStreamInfo, videoStreamFilePath);
+            string audioStreamFileExt = audioStreamInfo.Container.GetFileExtension();
+            string audioStreamFilePath = Path.Combine(OutputDir, $"AUD-{Guid.NewGuid()}.{audioStreamFileExt}");
+            await YoutubeClient.DownloadMediaStreamAsync(audioStreamInfo, audioStreamFilePath);
+
+            // Mux streams
+            Console.WriteLine("Combining...");
+            string outFilePath = Path.Combine(OutputDir, $"{cleanTitle}.mp4");
+            await FfmpegCli.ExecuteAsync($"-i \"{videoStreamFilePath}\" -i \"{audioStreamFilePath}\" -shortest \"{outFilePath}\" -y");
+
+            // Delete temp file
+            Console.WriteLine("Deleting temp files...");
+            File.Delete(videoStreamFilePath);
+            File.Delete(audioStreamFilePath);
+
+            Console.WriteLine($"Downloaded video [{id}] to [{outFilePath}]");
+        }
+
+        private static async Task DownloadPlaylistAsync(string id)
+        {
+            Console.WriteLine($"Working on playlist [{id}]...");
+
+            // Get playlist info
+            var playlistInfo = await YoutubeClient.GetPlaylistInfoAsync(id);
+            Console.WriteLine($"{playlistInfo.Title} ({playlistInfo.Videos.Count} videos)");
+
+            // Work on the videos
+            Console.WriteLine();
+            foreach (var video in playlistInfo.Videos)
+            {
+                await DownloadVideoAsync(video.Id);
+                Console.WriteLine();
+            }
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                // Try to determine the type of the URL/ID that was given
+
+                // Playlist ID
+                if (YoutubeClient.ValidatePlaylistId(arg))
+                {
+                    await DownloadPlaylistAsync(arg);
+                }
+
+                // Playlist URL
+                else if (YoutubeClient.TryParsePlaylistId(arg, out string playlistId))
+                {
+                    await DownloadPlaylistAsync(playlistId);
+                }
+
+                // Video ID
+                else if (YoutubeClient.ValidateVideoId(arg))
+                {
+                    await DownloadVideoAsync(arg);
+                }
+
+                // Video URL
+                else if (YoutubeClient.TryParseVideoId(arg, out string videoId))
+                {
+                    await DownloadVideoAsync(videoId);
+                }
+
+                // Unknown
+                else
+                {
+                    throw new ArgumentException($"Unrecognized URL or ID: [{arg}]", nameof(arg));
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Done");
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Title = "Youtube Music Downloader";
+
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+    }
+
+}
